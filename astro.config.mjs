b10162aebf76c5fe483e5ccb83fd309ce327c1eb
Wojciech Malarski <w.malarski@gmail.com>
2022-10-08 20:37:00 +0200

    localization added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import astroI18next from "astro-i18next";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()]
+  integrations: [tailwind(), partytown(), astroI18next()],
 });
